f1aa10086e4d0b853524f7edce39a4dc1d6e0c38
rda3mon <rda3mon@users.noreply.github.com>
2020-09-10 22:30:48 +0530

    support of favored nodes while compacting (#3)
    * support of favored nodes while compacting
    
    * version bump up

--- src/main/java/Compactor.java
+++ src/main/java/Compactor.java
@@ -1,15 +1,11 @@
 import config.CompactorConfig;
 import org.apache.commons.cli.MissingArgumentException;
-import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.exceptions.DeserializationException;
 import org.apache.log4j.Logger;
 import service.CompactionService;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 public class Compactor {
     private static Logger log = Logger.getLogger(Compactor.class);
@@ -19,9 +15,11 @@ public class Compactor {
             CompactorConfig compactorConfig = new CompactorConfig(args);
             Connection connection = ConnectionFactory.createConnection(compactorConfig.getHbaseConfig());
             new CompactionService(connection, compactorConfig).start();
-        } catch (IOException | InterruptedException  | MissingArgumentException e) {
+        } catch (MissingArgumentException e) {
             log.error(e);
-            e.printStackTrace();
+            System.exit(1);
+        } catch (IOException | InterruptedException e) {
+            log.error(e.getMessage(), e);
             System.exit(1);
         }
         System.exit(0);
