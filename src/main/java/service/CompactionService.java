87ff37465927f4ae5bfda100510920d51a3d30df
Mallikarjun <mallikvarjun@gmail.com>
2020-09-12 17:01:57 +0530

    handle catch all failure scenarios with treshold

--- src/main/java/service/CompactionService.java
+++ src/main/java/service/CompactionService.java
@@ -30,6 +30,7 @@ public class CompactionService {
   private int waitTime;
   private TableName tableName;
   private Map<String, RegionInfo> regionInfoMap = new HashMap<>();
+  private final int maxFailuresAllowed = 20;
 
   public CompactionService(Connection connection, CompactorConfig config) throws IOException {
     this.tableName = TableName.valueOf((String) config.getConfig(TABLE_NAME_KEY));
@@ -61,52 +62,65 @@ public class CompactionService {
     Set<String> compactingRegions = new HashSet<>();
     Set<String> completedRegions = new HashSet<>();
     int iteration = 0;
+    int remainingFailures = Math.max(maxFailuresAllowed, regionInfoMap.size()); //One run can fail these many times
     while (true) {
-      iteration += 1;
-      updateAllRegions();
-      log.info("Running iteration: " + iteration);
-      for (String compactingRegion : compactingRegions) {
-        CompactionState compactionState = admin.getCompactionStateForRegion(Bytes.toBytes(compactingRegion));
-        log.info("Compaction state: " + compactingRegion + " - " + compactionState);
-        if (compactionState.equals(CompactionState.MAJOR) || compactionState.equals(CompactionState.MAJOR_AND_MINOR)) {
-          log.info("In Progress compaction for: " + compactingRegion);
-        } else {
-          log.info("Compaction complete for: " + compactingRegion);
-          completedRegions.add(compactingRegion);
-        }
-      }
-      compactingRegions.removeAll(completedRegions);
+      try {
+        iteration += 1;
+        updateAllRegions();
+        log.info("Running iteration: " + iteration);
+        for (String compactingRegion : compactingRegions) {
+          CompactionState compactionState = admin.getCompactionStateForRegion(Bytes.toBytes(compactingRegion));
+          log.info("Compaction state: " + compactingRegion + " - " + compactionState);
+          if (compactionState.equals(CompactionState.MAJOR) || compactionState
+              .equals(CompactionState.MAJOR_AND_MINOR)) {
+            log.info("In Progress compaction for: " + compactingRegion);
+          } else {
+            log.info("Compaction complete for: " + compactingRegion);
+            completedRegions.add(compactingRegion);
+          }
 
-      List<String> regionsBatch = regionFetcher.getNextBatchOfEncodedRegions(compactingRegions);
-      log.info("Received regions in this batch of size: " + regionsBatch.size() + " Regions: " + regionsBatch);
-
-      if (regionsBatch.size() <= 0 && compactingRegions.size() <= 0) {
-        break; // exhausted all regions
-      }
-      for (String encodedRegion : regionsBatch) {
-        log.info("Starting compaction for: " + encodedRegion);
-        if (completedRegions.contains(encodedRegion) || compactingRegions.contains(encodedRegion)) {
-          log.info("Already completed or in progress: " + encodedRegion);
-          continue;
         }
-        if (regionInfoMap.containsKey(encodedRegion)) {
-          long lastCompactionTime =
-              admin.getLastMajorCompactionTimestampForRegion(regionInfoMap.get(encodedRegion).getRegionName());
-          if (!(boolean) compactorConfig.getConfig(FORCE_COMPACTION)
-              && getTimeAddHours(-1 * RECOMPACT_REGION_HOURS) < lastCompactionTime) {
-            log.info(
-                "Skipping compaction for " + encodedRegion + ". Already compacted at " + new Date(lastCompactionTime)
-                    + " which is later than " + new Date(getTimeAddHours(-1 * RECOMPACT_REGION_HOURS)));
+        compactingRegions.removeAll(completedRegions);
+
+        List<String> regionsBatch = regionFetcher.getNextBatchOfEncodedRegions(compactingRegions);
+        log.info("Received regions in this batch of size: " + regionsBatch.size() + " Regions: " + regionsBatch);
+
+        if (regionsBatch.size() <= 0 && compactingRegions.size() <= 0) {
+          break; // exhausted all regions
+        }
+        for (String encodedRegion : regionsBatch) {
+          log.info("Starting compaction for: " + encodedRegion);
+          if (completedRegions.contains(encodedRegion) || compactingRegions.contains(encodedRegion)) {
+            log.info("Already completed or in progress: " + encodedRegion);
             continue;
           }
+          if (regionInfoMap.containsKey(encodedRegion)) {
+            long lastCompactionTime =
+                admin.getLastMajorCompactionTimestampForRegion(regionInfoMap.get(encodedRegion).getRegionName());
+            if (!(boolean) compactorConfig.getConfig(FORCE_COMPACTION)
+                && getTimeAddHours(-1 * RECOMPACT_REGION_HOURS) < lastCompactionTime) {
+              log.info(
+                  "Skipping compaction for " + encodedRegion + ". Already compacted at " + new Date(lastCompactionTime)
+                      + " which is later than " + new Date(getTimeAddHours(-1 * RECOMPACT_REGION_HOURS)));
+              continue;
+            }
+          }
+          admin.majorCompactRegion(encodedRegion.getBytes());
+          log.info("Triggered compaction for: " + encodedRegion);
+          compactingRegions.add(encodedRegion);
         }
-        admin.majorCompactRegion(encodedRegion.getBytes());
-        log.info("Triggered compaction for: " + encodedRegion);
-        compactingRegions.add(encodedRegion);
+      } catch (Exception ex) {
+        log.error("Failed while running iteration: " + iteration + " remaining failures: " + remainingFailures
+            + " with error: " + ex.getMessage(), ex);
+        remainingFailures -= 1;
+        if (remainingFailures <= 0) {
+          log.warn("Aborting compaction run with some failures");
+          break;
+        }
       }
       log.info("Sleeping for " + this.waitTime);
       Thread.sleep(this.waitTime);
     }
-    log.info("compaction complete for regions: " + completedRegions);
+    log.info("compaction complete for regions: " + completedRegions + " in-progress for regions: " + compactingRegions);
   }
 }
