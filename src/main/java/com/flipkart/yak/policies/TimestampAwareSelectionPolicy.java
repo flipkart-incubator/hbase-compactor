47141f8097d864cb6341afb674c204f9965a7dec
Diptam Ghosh <diptam.ghosh@BLRETV-7GFQH2MF.local>
2023-09-28 13:29:11 +0530

    updated to report more metric and region starving handle

--- src/main/java/com/flipkart/yak/policies/TimestampAwareSelectionPolicy.java
+++ src/main/java/com/flipkart/yak/policies/TimestampAwareSelectionPolicy.java
@@ -1,6 +1,7 @@
 package com.flipkart.yak.policies;
 
 import com.flipkart.yak.config.CompactionContext;
+import com.flipkart.yak.core.MonitorService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
@@ -9,10 +10,7 @@ import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
 import org.apache.hadoop.hbase.util.Pair;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Simple implementation of {@link com.flipkart.yak.interfaces.RegionSelectionPolicy} that ignores a region as eligible
@@ -27,13 +25,26 @@ public class TimestampAwareSelectionPolicy extends NaiveRegionSelectionPolicy {
     @Override
     List<String> getEligibleRegions(Map<String, List<String>> regionFNHostnameMapping, Set<String> compactingRegions, List<RegionInfo> allRegions, Connection connection) throws IOException {
         List<String> regionsWhichCanBeCompacted = new ArrayList<>();
+        List<Pair<RegionInfo,Long>> sortedListOfRegionOnMCTime = new ArrayList<>();
         Admin admin = connection.getAdmin();
         long currentTimestamp = EnvironmentEdgeManager.currentTime();
         for(RegionInfo region: allRegions) {
             long timestampMajorCompaction = admin.getLastMajorCompactionTimestampForRegion(region.getRegionName());
-            log.debug("Region {} last compacted at {}", region, timestampMajorCompaction);
-            if ((currentTimestamp - timestampMajorCompaction > DELAY_BETWEEN_TWO_COMPACTIONS) && !compactingRegions.contains(region)) {
-                regionsWhichCanBeCompacted.add(region.getEncodedName());
+            if (timestampMajorCompaction > 0) {
+                sortedListOfRegionOnMCTime.add(new Pair<>(region, timestampMajorCompaction));
+            }
+        }
+        sortedListOfRegionOnMCTime.sort(Comparator.comparing(Pair::getSecond));
+        int size = sortedListOfRegionOnMCTime.size();
+        if (size > 0) {
+            long lowestTimeStamp = sortedListOfRegionOnMCTime.get(0).getSecond();
+            long highestTimeStamp = sortedListOfRegionOnMCTime.get(size -1).getSecond();
+            log.info("Region With Earliest timeStamp: {}, Region With Oldest timeStamp: {}", lowestTimeStamp, highestTimeStamp);
+        }
+        for(Pair<RegionInfo,Long> region: sortedListOfRegionOnMCTime) {
+            log.info("Region {} last compacted at {}", region, region.getSecond());
+            if ((currentTimestamp - region.getSecond() > DELAY_BETWEEN_TWO_COMPACTIONS) && !compactingRegions.contains(region)) {
+                regionsWhichCanBeCompacted.add(region.getFirst().getEncodedName());
             }
         }
         log.info("marked {} regions as eligible region ", regionsWhichCanBeCompacted.size());
