2b9b2a3895d7d08edea177a137ef4bbe8a245277
Diptam Ghosh <diptam.ghosh@BLRETV-7GFQH2MF.local>
2023-11-10 00:38:32 +0530

    updated to LinkedHashMap for Report

--- src/main/java/com/flipkart/yak/commons/Report.java
+++ src/main/java/com/flipkart/yak/commons/Report.java
@@ -6,11 +6,11 @@ import lombok.NonNull;
 import org.apache.hadoop.hbase.client.RegionInfo;
 import org.apache.hadoop.hbase.util.Pair;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 @Getter
 @AllArgsConstructor
-public class Report extends HashMap<String, Pair<RegionInfo, RegionEligibilityStatus>> {
+public class Report extends LinkedHashMap<String, Pair<RegionInfo, RegionEligibilityStatus>> {
     @NonNull
     String preparedBy;
 }
